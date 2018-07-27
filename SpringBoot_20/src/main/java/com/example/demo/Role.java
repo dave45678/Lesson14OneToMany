37a0e6e60e68648591e8c794a973fa750a32c57c
romielisse <romielisse@gmail.com>
2018-07-27 16:36:20 -0400

    changes

--- SpringBoot_20/src/main/java/com/example/demo/Role.java
+++ SpringBoot_20/src/main/java/com/example/demo/Role.java
@@ -1,40 +1,27 @@
 package com.example.demo;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
-@Table(name="USER")
-public class User {
+public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column (name = "email", nullable = false)
-    private String email;
+    @Column(unique=true)
+    private String role;
 
-    @Column(name = "password")
-    private String password;
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Collection<User> users;
 
-    @Column(name = "first_name")
-    private String firstName;
+    public Role() {
+    }
 
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "enabled")
-    private boolean enabled;
-
-    @Column (name = "username")
-    private String username;
-
-    @ManyToMany (fetch = FetchType.EAGER)
-    @JoinTable(joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Collection<Role> roles;
+    public Role(String role) {
+        this.role = role;
+    }
 
     public long getId() {
         return id;
@@ -44,66 +31,19 @@ public class User {
         this.id = id;
     }
 
-    public String getEmail() {
-        return email;
+    public String getRole() {
+        return role;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setRole(String role) {
+        this.role = role;
     }
 
-    public String getPassword() {
-
-        // I cheated... convert the password to a bcrypt password coming out.
-        // Stored in Db in clear text. Don't try this in production.
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String hashedPassword = passwordEncoder.encode(password);
-        System.out.println(hashedPassword);
-        this.password = hashedPassword;*/
-        return password;
+    public Collection<User> getUsers() {
+        return users;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Collection<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Collection<Role> roles) {
-        this.roles = roles;
+    public void setUsers(Collection<User> users) {
+        this.users = users;
     }
 }
